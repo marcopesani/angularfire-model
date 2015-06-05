ac223305ce2bffdffa0112a6758b7d7c3516b58d
Marco Pesani <marco.pesani@gmail.com>
2015-06-05 13:18:43 +0200

    Added Grunt for automation

---
+++ Gruntfile.js
@@ -0,0 +1,78 @@
+module.exports = function (grunt) {
+    'use strict';
+    // Project configuration
+    grunt.initConfig({
+        // Metadata
+        //pkg: grunt.file.readJSON('package.json'),
+        //banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+        //    '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+        //    '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+        //    '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+        //    ' Licensed <%= props.license %> */\n',
+
+        // Task configuration
+        concat: {
+            //options: {
+            //    banner: '<%= banner %>',
+            //    stripBanners: true
+            //},
+            dist: {
+                src: [
+                    'src/angularfire-model-validator.js',
+                    'src/angularfire-model.js'
+                ],
+                dest: 'dist/angularfire-model.js'
+            }
+        },
+        uglify: {
+            //options: {
+            //    banner: '<%= banner %>'
+            //},
+            dist: {
+                src: '<%= concat.dist.dest %>',
+                dest: 'dist/angularfire-model.min.js'
+            }
+        },
+        jshint: {
+            options: {
+                node: true,
+                curly: true,
+                eqeqeq: true,
+                immed: true,
+                latedef: true,
+                newcap: true,
+                noarg: true,
+                sub: true,
+                undef: true,
+                unused: true,
+                eqnull: true,
+                browser: true,
+                globals: {
+                    jQuery: true
+                },
+                boss: true
+            },
+            gruntfile: {
+                src: 'gruntfile.js'
+            },
+            lib_test: {
+                src: ['src/**/*.js', 'test/**/*.js']
+            }
+        },
+        watch: {
+            gruntfile: {
+                files: '<%= jshint.gruntfile.src %>',
+                tasks: ['jshint:gruntfile']
+            }
+        }
+    });
+
+    // These plugins provide necessary tasks
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Default task
+    grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'watch']);
+};
